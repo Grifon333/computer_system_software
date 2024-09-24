3f39c40af19730018f51b26bf6fff1d2e061c7e0
KroGen <daniil.king333@gmail.com>
2024-09-24 15:40:45 +0300

    Added tree optimization.

--- lib/ui/widgets/lab2/lab2_model.dart
+++ lib/ui/widgets/lab2/lab2_model.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:computer_system_software/library/lexical_analyzer.dart';
 import 'package:computer_system_software/library/syntax_analyzer.dart';
 import 'package:computer_system_software/library/token.dart';
@@ -9,12 +11,13 @@ import 'package:flutter/material.dart';
 class Lab2Model extends ChangeNotifier {
   Node? tree;
   String data = '';
+  String _previousData = '';
   AxisTree axisTree = AxisTree.vertical;
 
   String get axis => axisTree.name;
 
   void buildTree() {
-    if (data.isEmpty) return;
+    if (data.isEmpty || data == _previousData) return;
     tree = null;
     notifyListeners();
     LexicalAnalyzer lexicalAnalyzer = LexicalAnalyzer(
@@ -29,8 +32,13 @@ class Lab2Model extends ChangeNotifier {
     );
     tokens = syntaxAnalyzer.analyze();
     List<Token> list = _expressionToPost(tokens);
+    log(tokens.map((e) => e.value).join(''));
+    log(list.join());
     tree = _postToTree(list);
+    log(tree.toString());
+    tree = _optimizationTree(tree);
     notifyListeners();
+    _previousData = data;
   }
 
   List<Token> _expressionToPost(List<Token> tokens) {
@@ -123,6 +131,63 @@ class Lab2Model extends ChangeNotifier {
       default:
         return 6;
     }
+  }
+
+  Node? _optimizationTree(Node? node) {
+    if (node == null) return null;
+    while (true) {
+      if (node == null) break;
+      if (_isLeftRotate(node)) {
+        node = _leftRotate(node);
+      } else if (_isRightRotate(node)) {
+        node = _rightRotate(node);
+      } else {
+        break;
+      }
+    }
+    node?.leftChild = _optimizationTree(node.leftChild);
+    node?.rightChild = _optimizationTree(node.rightChild);
+    return node;
+  }
+
+  bool _isLeftRotate(Node node) {
+    int leftHeight = node.leftChild?.height ?? 0;
+    int rightHeight = node.rightChild?.height ?? 0;
+    return leftHeight - rightHeight > 1 &&
+        node.root.type == node.leftChild?.root.type;
+  }
+
+  bool _isRightRotate(Node node) {
+    int leftHeight = node.leftChild?.height ?? 0;
+    int rightHeight = node.rightChild?.height ?? 0;
+    return rightHeight - leftHeight > 1 &&
+        node.root.type == node.rightChild?.root.type;
+  }
+
+  Node? _leftRotate(Node? node) {
+    if (node == null) return null;
+    int leftLeftHeight = node.leftChild?.leftChild?.height ?? 0;
+    int leftRightHeight = node.leftChild?.rightChild?.height ?? 0;
+    if (leftRightHeight > leftLeftHeight &&
+        node.root.type == node.leftChild?.rightChild?.root.type) {
+      return node.bigRightRotate();
+    } else if (leftRightHeight < leftLeftHeight) {
+      return node.smallRightRotate();
+    }
+    return null;
+  }
+
+  Node? _rightRotate(Node? node) {
+    if (node == null) return null;
+    int rightLeftHeight = node.rightChild?.leftChild?.height ?? 0;
+    int rightRightHeight = node.rightChild?.rightChild?.height ?? 0;
+    if (rightLeftHeight > rightRightHeight &&
+        node.root.type == node.rightChild?.leftChild?.root.type) {
+      return node.bigLeftRotate();
+    } else if (rightLeftHeight < rightRightHeight) {
+      return node.smallLeftRotate();
+    }
+    return null;
   }
 
   void onChangeData(String value) {
