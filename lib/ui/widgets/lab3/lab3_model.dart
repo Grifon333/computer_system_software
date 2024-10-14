ab828a66ba95a175ba47fccf9bf74d992fde8f0c
KroGen <daniil.king333@gmail.com>
2024-10-14 09:13:50 +0300

    Refactoring

--- lib/ui/widgets/lab3/lab3_model.dart
+++ lib/ui/widgets/lab3/lab3_model.dart
@@ -1,73 +1,120 @@
+import 'package:computer_system_software/domain/entities/tree.dart';
 import 'package:computer_system_software/domain/repositories/expression_analyzer_repository.dart';
 import 'package:computer_system_software/domain/repositories/expression_tree_repository.dart';
 import 'package:computer_system_software/library/lexical_analyzer/token.dart';
 import 'package:flutter/material.dart';
 
 class Lab3Model extends ChangeNotifier {
+  String data = '';
+  List<dynamic> permutations = [];
+  Tree? initialTree;
+  Tree? resultTree;
+  Map<String, List<List<dynamic>>> perms = {};
+  int index = 0;
+
+  int countLoops = 0; // TODO: remove
+
   final ExpressionAnalyzerRepository _expressionAnalyzerRepository =
       ExpressionAnalyzerRepository();
   final ExpressionTreeRepository _expressionTreeRepository =
       ExpressionTreeRepository();
 
-  /*
-  a*b-(d*c*e)+(k*m-y+i*j-n)*p*o+x-f
-  a*b-d*c*e+k*m-y+i*j-n*p*o+x-f
-  a*b+(d*c*e)+(k*m+y+i*j+n)*p*o+x+f
-  a+b+(c+d)
-  a+b+(c+d)*(e+f)
-   */
+  List<List<String>> getPermutations(List<String> items) {
+    if (items.isEmpty) return [[]];
+    return items
+        .expand((item) => getPermutations(List.from(items)..remove(item))
+            .map((perm) => [item] + perm))
+        .toList();
+  }
 
-  void init() {
-    String data = 'a*b+(d*c*e)+(k*m+y+i*j+n)*p*o+x+f'.trim();
+  void buildTree() {
+    data = data.trim();
     if (data == '') return;
-    final tokens = _expressionAnalyzerRepository.analyze(data);
-    final result = _splitTerms(tokens);
-    final res = _permutation(result).map((el) => _joinTokens(el).join());
-    final set = res.toSet();
-    debugPrint(set.join('\n'));
-    debugPrint('Count: ${res.length}');
-    debugPrint('Count of Set: ${set.length}');
+    perms.clear();
+    index = 0;
+    permutations.clear();
+    initialTree = null;
+    resultTree = null;
+    countLoops = 0;
+    // Stopwatch stopwatch = Stopwatch()..start();
+    List<Token> tokens = _expressionAnalyzerRepository.analyze(data);
+    String oldData = data;
+    while (true) {
+      initialTree = _expressionTreeRepository.build(tokens);
+      data = _expressionTreeRepository.treeToExpression(initialTree);
+      tokens = _expressionAnalyzerRepository.analyze(data);
+      if (oldData == data) break;
+      oldData = data;
+    }
+
+    int height = initialTree?.height ?? 0;
+    // debugPrint('Analyzer time: ${stopwatch.elapsed.inMilliseconds} ms');
+    // stopwatch.reset();
+    final splitTokens = _splitTerms(tokens);
+    permutations = _permutation(splitTokens)
+        .map((el) => _joinTokens(el).join())
+        .toSet()
+        .toList();
+    // debugPrint('Permutation time: ${stopwatch.elapsed.inMilliseconds} ms');
+    debugPrint('Count Loops: $countLoops');
+    debugPrint('Old height: $height');
+    // stopwatch.reset();
+    int expIndex = 0;
+    for (int i = 0; i < permutations.length; i++) {
+      final t = _expressionTreeRepository.build(
+        _expressionAnalyzerRepository.analyze(permutations[i]),
+      );
+      int h = t?.height ?? 0;
+      if (h < height) {
+        height = h;
+        resultTree = t;
+        expIndex = i;
+      }
+    }
+    resultTree ??= _expressionTreeRepository
+        .build(_expressionAnalyzerRepository.analyze(permutations[expIndex]));
+    debugPrint('New height: $height');
+    debugPrint('Expression: (${expIndex + 1})  ${permutations[expIndex]}');
+    // debugPrint('Build Trees time: ${stopwatch.elapsed.inMilliseconds} ms');
+    debugPrint('----------------------------------------------\n');
+    notifyListeners();
   }
 
   List<Token> _joinTokens(List<dynamic> list) {
-    List<Token> tokens = [];
-    for (var el in list) {
-      if (el is Token) {
-        tokens.add(el);
-      } else {
-        tokens.addAll(_joinTokens(el));
-      }
-    }
-    return tokens;
+    return list.expand((el) => el is Token ? [el] : _joinTokens(el)).toList();
   }
-
-  Map<String, List<List<dynamic>>> perms = {};
 
   List<List<dynamic>> _permutation(List<dynamic> list) {
     List<dynamic> arr = [...list];
     List<List<dynamic>> result = [
       [...list]
     ];
-    var (innerPerms, indexes, countLists) = _innerPermutationsList(list);
+    var (innerPerms, indexes, lists) = _innerPermutationsList(list);
     final int n = arr.length;
+
+    result.addAll(_innerPermutations(innerPerms, arr, indexes, true));
+    if (lists.length < 2) return result;
+
     final List<int> p = List.generate(n, (i) => i);
     int i = 1;
 
-    result.addAll(_innerPermutations(innerPerms, arr, indexes, true));
-    if (countLists < 2) return result;
-
     while (i < n) {
+      countLoops++;
       p[i]--;
       int j = (i % 2) * p[i];
-      if (arr[i] is List<dynamic> && arr[j] is List<dynamic>) {
+      if (arr[i] is List &&
+          arr[j] is List &&
+          !_equalsArraysLength(arr[i], arr[j])) {
         final t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
-        result.add([...arr]);
-        final oldIndexes = [...indexes];
-        if (oldIndexes.contains(i)) indexes[oldIndexes.indexOf(i)] = j;
-        if (oldIndexes.contains(j)) indexes[oldIndexes.indexOf(j)] = i;
-        result.addAll(_innerPermutations(innerPerms, arr, indexes));
+        if (!result.any((el) => _equalsArraysLength(el, arr))) {
+          result.add([...arr]);
+          final oldIndexes = [...indexes];
+          if (oldIndexes.contains(i)) indexes[oldIndexes.indexOf(i)] = j;
+          if (oldIndexes.contains(j)) indexes[oldIndexes.indexOf(j)] = i;
+          result.addAll(_innerPermutations(innerPerms, arr, indexes));
+        }
       }
       i = 1;
       while (i < n && p[i] == 0) {
@@ -78,6 +125,18 @@ class Lab3Model extends ChangeNotifier {
     return result;
   }
 
+  bool _equalsArraysLength(List<dynamic> a, List<dynamic> b) {
+    if (a.length != b.length) return false;
+    for (int i = 0; i < a.length; i++) {
+      if ((a[i] is List && b[i] is List && !_equalsArraysLength(a[i], b[i])) ||
+          (a[i] is! List && b[i] is List) ||
+          (a[i] is List && b[i] is! List)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   List<List<dynamic>> _innerPermutations(
     List<List<dynamic>> innerPermutations,
     List<dynamic> list,
@@ -86,7 +145,7 @@ class Lab3Model extends ChangeNotifier {
   ]) {
     if (innerPermutations.isEmpty) return [];
     List<List<dynamic>> result = [];
-    generate(int depth) {
+    void generate(int depth) {
       if (depth == innerPermutations.length) return result.add([...list]);
       for (List<dynamic> perm in innerPermutations[depth]) {
         list[indexes[depth]] = perm;
@@ -97,56 +156,63 @@ class Lab3Model extends ChangeNotifier {
     generate(0);
     if (isSkipFirst) result.removeAt(0);
     return result;
-    // return removeDuplicates(result);
   }
 
   (
     List<List<dynamic>> innerPermutations,
     List<int> indexes,
-    int countLists,
+    List<int> countLists,
   ) _innerPermutationsList(List<dynamic> list) {
     List<List<dynamic>> innerPermutations = [];
     List<int> indexes = [];
-    int countLists = 0;
+    Set<int> listsSet = {};
     for (int i = 0; i < list.length; i++) {
-      final el = list[i];
-      if (el is List<dynamic>) {
-        countLists++;
-        if (!perms.containsKey(el.toString())) {
-          perms[el.toString()] = _permutation(el);
+      if (list[i] is List<dynamic>) {
+        String key = list[i].toString();
+        listsSet.add((list[i] as List).length);
+        if (!perms.containsKey(key)) {
+          perms[key] = _permutation(list[i]);
         }
-        if (perms[el.toString()]?.length == 1) continue;
-        innerPermutations.add(perms[el.toString()]!);
+        if (perms[key]?.length == 1) continue;
+        innerPermutations.add(perms[key]!);
         indexes.add(i);
       }
     }
-    return (innerPermutations, indexes, countLists);
+    if (listsSet.length == 1) listsSet.clear();
+    final lists = listsSet.toList();
+    lists.sort();
+    return (innerPermutations, indexes, lists);
   }
-
-  int index = 0;
 
   List<dynamic> _splitTerms(List<Token> tokens) {
     List<dynamic> terms = [];
     List<dynamic> term = [];
-    term.add(tokens[index]);
+    int numbersCount = 0;
     if (!tokens[index].isPlusMinus) {
-      term.insert(0, const Token(type: TokenType.plus_minus, value: '+'));
+      term.add(const Token(type: TokenType.plus_minus, value: '+'));
     }
-    index++;
     for (; index < tokens.length; index++) {
       final token = tokens[index];
+      if (_isNumber(token.value)) numbersCount++;
       if (token.isPlusMinus) {
         terms.add(term);
         term = [token];
       } else if (token.isLeftBracket) {
         index++;
         List<dynamic> innerTerms = _splitTerms(tokens);
-        while (true) {
-          if (innerTerms.length != 1) break;
-          innerTerms = innerTerms.first;
-        }
-        if (innerTerms.length == 1) {
-          term.add(innerTerms.first);
+        if (term.last is Token &&
+            term.last.value == '+' &&
+            innerTerms.length == 2 &&
+            innerTerms[1] is Token) {
+          term.removeLast();
+          term.addAll(innerTerms);
+          if (_isNumber(innerTerms[1].value)) numbersCount++;
+        } else if (term.last is Token &&
+            term.last.value == '-' &&
+            innerTerms.length == 2 &&
+            innerTerms[1] is Token) {
+          term.add(innerTerms[1]);
+          if (_isNumber(innerTerms[1].value)) numbersCount++;
         } else {
           term.addAll([
             token,
@@ -161,37 +227,42 @@ class Lab3Model extends ChangeNotifier {
       }
     }
     terms.add(term);
-    if (term.length == 1) return terms.first;
+
+    if (numbersCount > 1) {
+      double number = 0;
+      List<List<dynamic>> newTerms = [];
+      for (var t in terms) {
+        if (t is List && t.length == 2 && _isNumber(t[1].value)) {
+          final sign = t[0] as Token;
+          final n = t[1] as Token;
+          if (sign.value == '-') {
+            number -= double.parse(n.value);
+          } else {
+            number += double.parse(n.value);
+          }
+        } else {
+          newTerms.add(t);
+        }
+      }
+      if (number != 0) {
+        newTerms.add([
+          Token(type: TokenType.plus_minus, value: number > 0 ? '+' : '-'),
+          Token(type: TokenType.number_variable, value: '${number.abs()}'),
+        ]);
+      }
+      terms = newTerms;
+    }
+
+    if (terms.length == 1) return terms.first;
     return terms;
   }
 
-  void onPressed() {
-    init();
+  bool _isNumber(String? value) {
+    if (value == null) return false;
+    return double.tryParse(value) != null;
   }
-}
 
-/*
-bool deepEquals(dynamic a, dynamic b) {
-  if (a is List && b is List) {
-    if (a.length != b.length) return false;
-    for (int i = 0; i < a.length; i++) {
-      if (!deepEquals(a[i], b[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return a == b;
-}
+  void onPressed() => buildTree();
 
-List<List<dynamic>> removeDuplicates(List<List<dynamic>> input) {
-  List<List<dynamic>> result = [];
-  for (var item in input) {
-    bool isDuplicate = result.any((existing) => deepEquals(existing, item));
-    if (!isDuplicate) {
-      result.add(item);
-    }
-  }
-  return result;
+  void onChangeData(String value) => data = value;
 }
- */
--- lib/ui/widgets/lab3/view/lab3_page.dart
+++ lib/ui/widgets/lab3/view/lab3_page.dart
@@ -1,3 +1,4 @@
+import 'package:computer_system_software/library/painters/binary_tree_painter/binary_tree_painter.dart';
 import 'package:computer_system_software/ui/widgets/lab3/lab3.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,7 @@ class Lab3Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Lab 3')),
+      appBar: AppBar(title: const Text('Lab 3 (commutative law)')),
       body: const _Body(),
     );
   }
@@ -20,18 +21,172 @@ class _Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: double.infinity,
-        height: 50,
-        child: ElevatedButton(
-          onPressed: context.read<Lab3Model>().onPressed,
-          child: const Text(
-            'Press',
-            style: TextStyle(fontSize: 24),
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            const TextForm(),
+            const SizedBox(height: 20),
+            const _Permutations(),
+            const SizedBox(height: 20),
+            LayoutBuilder(builder: (
+              BuildContext context,
+              BoxConstraints constraints,
+            ) {
+              final maxWidth = constraints.maxWidth;
+              if (constraints.maxWidth < 600) {
+                return Column(
+                  children: [
+                    InitialTree(maxWidth: maxWidth),
+                    const SizedBox(height: 20),
+                    ResultTree(maxWidth: maxWidth),
+                  ],
+                );
+              }
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InitialTree(maxWidth: maxWidth),
+                  const SizedBox(width: 40),
+                  ResultTree(maxWidth: maxWidth),
+                ],
+              );
+            }),
+          ],
+        ));
+  }
+}
+
+class TextForm extends StatelessWidget {
+  const TextForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<Lab3Model>();
+    return Column(
+      children: [
+        TextField(
+          minLines: 1,
+          maxLines: 2,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            hintText: 'Enter expression',
+          ),
+          style: const TextStyle(fontSize: 20),
+          onChanged: model.onChangeData,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: model.onPressed,
+            child: const Text('build tree', style: TextStyle(fontSize: 16)),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Permutations extends StatelessWidget {
+  const _Permutations();
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<Lab3Model>();
+    final permutations = model.permutations;
+    final count = permutations.length;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Count of permutations: $count',
+          style: const TextStyle(fontSize: 16),
+        ),
+        const SizedBox(height: 8),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black54),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+          child: ExpansionTile(
+            title: const Text(
+              'Permutations',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            childrenPadding: const EdgeInsets.all(8),
+            children: List.generate(
+              permutations.length,
+              (i) => Text('${i + 1}) ${permutations[i]}'),
+            ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+enum BinaryTreeType { initial, result }
+
+class BinaryTree extends StatelessWidget {
+  final BinaryTreeType type;
+  final double maxWidth;
+
+  const BinaryTree({super.key, required this.type, required this.maxWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    final tree = context.select(
+      (Lab3Model model) =>
+          type == BinaryTreeType.initial ? model.initialTree : model.resultTree,
+    );
+    if (tree == null) return const SizedBox.shrink();
+    double width = 0;
+    if (maxWidth < 600) {
+      width = MediaQuery.of(context).size.width - 34;
+    } else {
+      width = (MediaQuery.of(context).size.width - 34 - 40) / 2;
+    }
+
+    const axis = AxisTree.vertical;
+    int countNodesInWight =
+        axis == AxisTree.vertical ? tree.countOfLeaves : tree.height;
+    double x = width / (5 * countNodesInWight - 1);
+    double radius = 2 * x;
+    double gap = x;
+    double height = (2 * radius + gap) *
+            (axis == AxisTree.horizontal ? tree.countOfLeaves : tree.height) -
+        gap;
+
+    return CustomPaint(
+      size: Size(width, 1 * height),
+      painter: BinaryTreePainter(
+        tree: tree,
+        width: width,
+        axis: axis,
       ),
     );
+  }
+}
+
+class InitialTree extends StatelessWidget {
+  final double maxWidth;
+
+  const InitialTree({super.key, required this.maxWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    return BinaryTree(type: BinaryTreeType.initial, maxWidth: maxWidth);
+  }
+}
+
+class ResultTree extends StatelessWidget {
+  final double maxWidth;
+
+  const ResultTree({super.key, required this.maxWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    return BinaryTree(type: BinaryTreeType.result, maxWidth: maxWidth);
   }
 }
